0b036ccdf7fea15c5a24909ac03a3f1ffae99740
Tim Andersen <timander@gmail.com>
2015-04-05 12:22:52 -0500

    added test data

---
+++ src/test/resources/sql/migrations/V005.1__insert_customers.sql
@@ -0,0 +1,5 @@
+insert into people (first_name, last_name, address_line_1, address_line_2, city, state, zip_code)
+  values ('John', 'Doe', '123 Main', 'Apt #2', 'Des Moines', 'IA', '50265');
+
+insert into people (first_name, last_name, address_line_1, address_line_2, city, state, zip_code)
+  values ('Jane', 'Smith', '789 Spruce', null, 'De Soto', 'IA', '50069');
